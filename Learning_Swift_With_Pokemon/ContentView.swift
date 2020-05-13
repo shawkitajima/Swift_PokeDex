718ef16ef389ed5f63d6f0196169993ef1cc48e2
Shaw Kitajima <shawkitajima@gmail.com>
2020-05-13 13:33:07 -0700

    Initial Commit

---
+++ Learning_Swift_With_Pokemon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning_Swift_With_Pokemon
+//
+//  Created by Shaw Kitajima on 5/13/20.
+//  Copyright © 2020 Shaw Kitajima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning_Swift_With_PokemonTests/Learning_Swift_With_PokemonTests.swift
@@ -0,0 +1,34 @@
+//
+//  Learning_Swift_With_PokemonTests.swift
+//  Learning_Swift_With_PokemonTests
+//
+//  Created by Shaw Kitajima on 5/13/20.
+//  Copyright © 2020 Shaw Kitajima. All rights reserved.
+//
+
+import XCTest
+@testable import Learning_Swift_With_Pokemon
+
+class Learning_Swift_With_PokemonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
